23093904ebddaab36e8b4fac9bda70fab9b8ba54
UpSetSummerSet <72717739+UpSetSummerSet@users.noreply.github.com>
2020-11-11 04:57:17 +0800

    Continue

---
+++ AniTest_15_TanKeeLiang/Assets/Scripts/PlayerController15.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController15 : MonoBehaviour
+{
+    Animator playerAnim;
+
+    float speed = 5.00f;
+    float Bspeed = 10.00f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            playerAnim.SetBool("isStrafe", true);
+        }
+        else if(Input.GetKeyUp(KeyCode.W))
+        {
+            playerAnim.SetBool("isStrafe", false);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * -Bspeed);
+            playerAnim.SetBool("isStrafe", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            playerAnim.SetBool("isStrafe",false);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * -speed);
+            playerAnim.SetBool("isStrafe", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.A))
+        {
+            playerAnim.SetBool("isStrafe", false);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+            playerAnim.SetBool("isStrafe", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            playerAnim.SetBool("isStrafe", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerAnim.SetTrigger("trigAttack");
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Cube")
+        {
+            playerAnim.SetTrigger("trigDeath");
+        }
+    }
+}
